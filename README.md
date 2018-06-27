d2414b448be9ec97012dd4cbb7caf1244a59c9c2
maodq <noiary@126.com>
2018-06-27 14:04:37 +0800

    rm build dir

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A new Flutter project.
 
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
+
+模仿亦播客，是一个视频类项目
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ybk/app.dart';
 
 import 'package:ybk/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new YbkApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
